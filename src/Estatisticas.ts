c48f55a9d4dc152334e3c28f983f528764003d96
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-12 19:07:44 -0300

    setSemana

--- src/Estatisticas.ts
+++ src/Estatisticas.ts
@@ -10,11 +10,13 @@ export default class Estatisticas {
   total;
   pagamento;
   status;
+  semana;
   constructor(transacoes: Transacao[]) {
     this.transacoes = transacoes;
     this.total = this.setTotal();
     this.pagamento = this.setPagamento();
     this.status = this.setStatus();
+    this.semana = this.setSemana();
   }
   private setTotal() {
     return this.transacoes.filter(filtrarValor).reduce((acc, item) => {
@@ -26,5 +28,45 @@ export default class Estatisticas {
   }
   private setStatus() {
     return countBy(this.transacoes.map(({ status }) => status));
+  }
+  private setSemana() {
+    const semana = {
+      ['Domingo']: 0,
+      ['Segunda']: 0,
+      ['Terça']: 0,
+      ['Quarta']: 0,
+      ['Quinta']: 0,
+      ['Sexta']: 0,
+      ['Sábado']: 0,
+    };
+    for (let i = 0; i < this.transacoes.length; i++) {
+      const day = this.transacoes[i].data.getDay();
+      switch (day) {
+        case 0:
+          semana.Domingo += 1;
+          break;
+        case 1:
+          semana.Segunda += 1;
+          break;
+        case 2:
+          semana.Terça += 1;
+          break;
+        case 3:
+          semana.Quarta += 1;
+          break;
+        case 4:
+          semana.Quinta += 1;
+          break;
+        case 5:
+          semana.Sexta += 1;
+          break;
+        case 6:
+          semana.Sábado += 1;
+          break;
+        default:
+          break;
+      }
+    }
+    return semana;
   }
 }
